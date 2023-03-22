503d14e1a007d983660011f32f236b065d67313b
Markusha <111707939+Markussia@users.noreply.github.com>
2023-03-22 23:06:51 +0900

    update 1.4

--- HookCode/js/script.js
+++ HookCode/js/script.js
@@ -21,7 +21,12 @@ let labelVue = document.querySelector('#labelVue');
 let questP = document.querySelector('#questP');
 let choiceRadio = document.querySelector('.choice__option__radio');
 let choiceText = document.querySelector('.choice__option__text');
-let inpQuiz = document.querySelector('#inpQuiz')
+let inpQuiz = document.querySelector('#inpQuiz');
+let result1 = document.querySelector('#result');
+
+let balls = 0
+let ballsFake = 0
+
 const fullDashArray = 283;
 const warning = 10;
 const alert = 5;
@@ -30,116 +35,6 @@ const timeLimit = 420;
 let timePassed = 0;
 let timeLeft = timeLimit;
 let timerInterval = null;
-
-numberQuest.innerHTML = "Вопрос: " +
-    number + " / 5"
-
-test0();
-
-function test0() {
-    if (number == 1) {
-        choiceRadio.style.display = ''
-        choiceText.style.display = 'none'
-    }
-}
-
-
-function test1() {
-    if (css__radio.checked === true) {
-        console.log("да")
-    } else {
-        console.log('неправильно')
-    }
-}
-
-function test2() {
-    if (js__radio.checked === true) {
-        console.log("да")
-    } else {
-        console.log('неправильно')
-    }
-}
-
-function test3() {
-    if (inpQuiz.value == 'npm') {
-        console.log("да")
-    } else if (inpQuiz.value == 'NPM') {
-        console.log("да")
-    } else if (inpQuiz.value == 'Npm') {
-        console.log("да")
-    } else {
-        console.log('неправильно')
-    }
-}
-
-function test4() {
-    if (inpQuiz.value == 'scss') {
-        console.log("да")
-    } else if (inpQuiz.value == 'SCSS') {
-        console.log("да")
-    } else {
-        console.log('неправильно')
-    }
-}
-
-function test5() {
-    if (inpQuiz.value == 'Нет') {
-        console.log("да")
-    } else if (inpQuiz.value == 'нет') {
-        console.log("да")
-    } else {
-        console.log('неправильно')
-    }
-}
-
-
-
-answer__btn.addEventListener("click", function() {
-    number = number + 1
-    numberQuest.innerHTML = "Вопрос: " +
-        number + " / 5 "
-    if (number > 5) {
-        answer__btn.style.display = 'none'
-    }
-
-    if (number == 2) {
-        test1();
-        questP.innerHTML = 'С помощью какого фрейворка можно писать backend и frontend? '
-        labelHtml.innerHTML = 'JS'
-        labelCss.innerHTML = 'React'
-        labelJs.innerHTML = 'Next.js'
-        labelVue.innerHTML = 'tmi.js'
-        choiceRadio.style.display = ''
-    }
-    if (number == 3) {
-        test2();
-        choiceRadio.style.display = 'none'
-        choiceText.style.display = ''
-        questP.innerHTML = 'С помощью чего можно установить Vue?<br>.... install vue'
-
-    }
-    if (number == 4) {
-        test3();
-        questP.innerHTML = 'Как называется популярный препроцессор для CSS?<br>S...'
-        inpQuiz.value = ''
-    }
-    if (number == 5) {
-        test4();
-        questP.innerHTML = 'HTML - это язык програмиирования?<br>Да/Нет'
-        inpQuiz.value = ''
-
-
-    }
-    if (number == 6) {
-        test5();
-        outQuiz.style.display = 'block'
-        numberQuest.style.display = 'none'
-        questBlock.style.display = 'none'
-    }
-
-    test0();
-
-})
 
 
 
@@ -196,12 +91,142 @@ function formatTime(time) {
     return `${minutes}:${seconds}`;
 }
 
+
+numberQuest.innerHTML = "Вопрос: " +
+    number + " / 5"
+
+test0();
+
+function test0() {
+    if (number == 1) {
+        choiceRadio.style.display = ''
+        choiceText.style.display = 'none'
+    }
+}
+
+
+function test1() {
+    if (css__radio.checked === true) {
+        balls += 1
+        ballsFake += 3
+        localStorage.setItem('test', balls);
+    } else {}
+}
+
+function test2() {
+    if (js__radio.checked === true) {
+        balls += 1
+        ballsFake += 4
+    } else {}
+}
+
+function test3() {
+    if (inpQuiz.value == 'npm') {
+        balls += 1
+        ballsFake += 3
+    } else if (inpQuiz.value == 'NPM') {
+        balls += 1
+        ballsFake += 3
+    } else if (inpQuiz.value == 'Npm') {
+        balls += 1
+        ballsFake += 3
+    } else {}
+}
+
+function test4() {
+    if (inpQuiz.value == 'scss') {
+        balls += 1
+        ballsFake += 2
+    } else if (inpQuiz.value == 'SCSS') {
+        balls += 1
+        ballsFake += 2
+    } else {}
+}
+
+function test5() {
+    if (inpQuiz.value == 'Нет') {
+        balls += 1
+        ballsFake += 3
+    } else if (inpQuiz.value == 'нет') {
+        balls += 1
+        ballsFake += 3
+    } else {}
+}
+
+
+
+answer__btn.addEventListener("click", function() {
+    number = number + 1
+    numberQuest.innerHTML = "Вопрос: " +
+        number + " / 5 "
+    if (number > 5) {
+        answer__btn.style.display = 'none'
+    }
+
+    if (number == 2) {
+        test1();
+        questP.innerHTML = 'С помощью какого фрейворка можно писать backend и frontend? '
+        labelHtml.innerHTML = 'JS'
+        labelCss.innerHTML = 'React'
+        labelJs.innerHTML = 'Next.js'
+        labelVue.innerHTML = 'tmi.js'
+        choiceRadio.style.display = ''
+    }
+    if (number == 3) {
+        test2();
+        choiceRadio.style.display = 'none'
+        choiceText.style.display = ''
+        questP.innerHTML = 'С помощью чего можно установить Vue?<br>.... install vue'
+
+    }
+    if (number == 4) {
+        test3();
+        questP.innerHTML = 'Как называется популярный препроцессор для CSS?<br>S...'
+        inpQuiz.value = ''
+    }
+    if (number == 5) {
+        test4();
+        questP.innerHTML = 'HTML - это язык програмиирования?<br>Да/Нет'
+        inpQuiz.value = ''
+    }
+    if (number == 6) {
+        test5();
+        outQuiz.style.display = 'block'
+        numberQuest.style.display = 'none'
+        questBlock.style.display = 'none'
+        inpQuiz.value = ''
+        onTimesUp();
+        if (balls == 5) {
+            balls = 5
+        }
+
+        if (balls == 4) {
+            balls = 4
+        }
+
+        if (balls == 3) {
+            balls = 3
+        }
+
+        if (balls <= 2) {
+            balls = 2
+        }
+    }
+
+    test0();
+})
+
+
+
+
 btnLocationURL.addEventListener('click', function() {
-    window.location = 'result.html'
+    btnLocationURL.style.display = 'none'
 
 })
 
 btnLocationURL2.addEventListener('click', function() {
-    window.location = 'wesult.html'
-
+    btnLocationURL2.style.display = 'none'
+    result1.style.display = 'block'
+    result1.innerHTML = "У вас баллов: " +
+        ballsFake + "<br>Ваша оценка: " + balls
 })
